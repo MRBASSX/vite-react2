71d2b04680bf201707db5d11188ffb32c9f9da30
mrbassx <abassiddrisu9222@gmail.com>
2025-07-23 21:54:45 +0000

    up

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -24,20 +24,28 @@ export default function Register() {
 
 
   const handleSignIn = async () => {
-  const response = await fetch('https://express-and-react.vercel.app/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-    username: 'Fred',
-    email: 'Flintstone@gmail.com',
-    password:'test123456789'
-  })
-  });
 
-  const result = await response.json();
-  console.log(result);
+  try {
+    const response = await fetch('https://express-and-react.vercel.app/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+      username: 'Fred',
+      email: 'Flintstone@gmail.com',
+      password:'test123456789'
+    })
+    });
+  
+    const data = await response.json();
+      console.log('Response from backend:', data);
+  } catch (error) {
+
+     console.error('Error:', error);
+    
+  }
+
 };
 
 
