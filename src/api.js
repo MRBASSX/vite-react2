542144638c5440f7678452ebef7669b05005ba86
mrbassx <abassiddrisu9222@gmail.com>
2025-07-24 03:05:02 +0000

    updated..

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 
-const csrfToken = document.querySelector('meta[name="csrf-token"]');
+// const csrfToken = document.querySelector('meta[name="csrf-token"]');
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
